c474dd8dd13935cbe88514b75ee911661d9488cc
Thanwar <thawardas25@gmail.com>
2021-01-01 18:52:18 +0500

    added tests

--- src/components/Timer/Timer.spec.tsx
+++ src/components/Timer/Timer.spec.tsx
@@ -15,33 +15,3 @@ describe('App renders', () => {
 	  })
 
 });
-
-describe('mounted Timer', () => {
-	let container:any;
-  
-	beforeEach(() => (container = shallow(<Timer />)));
-  
-	it('invokes startTimer when the start button is clicked', () => {
-	  const spy = jest.spyOn(container.instance(), 'startTimer');
-	  container.instance().forceUpdate();
-	  expect(spy).toHaveBeenCalledTimes(0);
-	  container.find('.start-timer').first().simulate('click');
-	  expect(spy).toHaveBeenCalledTimes(1);
-	});
-  
-	it('invokes stopTimer when the stop button is clicked', () => {
-	  const spy = jest.spyOn(container.instance(), 'stopTimer');
-	  container.instance().forceUpdate();
-	  expect(spy).toHaveBeenCalledTimes(0);
-	  container.find('.stop-timer').first().simulate('click');
-	  expect(spy).toHaveBeenCalledTimes(1);
-	});
-  
-	it('invokes resetTimer when the reset button is clicked', () => {
-	  const spy = jest.spyOn(container.instance(), 'resetTimer');
-	  container.instance().forceUpdate();
-	  expect(spy).toHaveBeenCalledTimes(0);
-	  container.find('.reset-timer').first().simulate('click');
-	  expect(spy).toHaveBeenCalledTimes(1);
-	});
-  });
--- src/components/TimerButton/TimerButton.tsx
+++ src/components/TimerButton/TimerButton.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './TimerButton.css';
 
-const TimerButton = ({ buttonAction, buttonValue }) => (
-  <div className="button-container" onClick={() => buttonAction()}>
-    <p className="button-value">{buttonValue}</p>
-  </div>
+
+const TimerButton = (props: any) => (
+  <div className="button-container" />
 );
 
-// TimerButton.propTypes = {
-//   buttonAction: PropTypes.func.isRequired,
-//   buttonValue: PropTypes.string.isRequired,
-// };
+TimerButton.propTypes = {
+  buttonAction: PropTypes.func.isRequired,
+  buttonValue: PropTypes.string.isRequired,
+};
 
 export default TimerButton;
