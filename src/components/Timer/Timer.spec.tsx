ce4cd8fcb8c6897915c05048ac5f51c1634fbe1e
Thanwar <thawardas25@gmail.com>
2021-01-02 14:48:28 +0500

    added tests

--- src/components/Timer/Timer.spec.tsx
+++ src/components/Timer/Timer.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount, ReactWrapper, shallow } from 'enzyme';
 import Timer from './Timer';
 
 describe('App renders', () => {
@@ -15,3 +15,34 @@ describe('App renders', () => {
 	  })
 
 });
+
+describe('mounted Timer', () => {
+	let container: ReactWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
+  
+	beforeEach(() => (container = shallow(<Timer />)));
+  
+	it('invokes startTimer when the start button is clicked', () => {
+	  const spy = jest.spyOn(container.instance(), 'startTimer');
+	  container.instance().forceUpdate();
+	  expect(spy).toHaveBeenCalledTimes(0);
+	  container.find('.start-timer').first();
+	  expect(spy).toHaveBeenCalledTimes(0);
+	});
+  
+	it('invokes stopTimer when the stop button is clicked', () => {
+	  const spy = jest.spyOn(container.instance(), 'stopTimer');
+	  container.instance().forceUpdate();
+	  expect(spy).toHaveBeenCalledTimes(0);
+	  container.find('.stop-timer');
+	  expect(spy).toHaveBeenCalledTimes(0);
+	});
+  
+	it('invokes resetTimer when the reset button is clicked', () => {
+	  const spy = jest.spyOn(container.instance(), 'resetTimer');
+	  container.instance().forceUpdate();
+	  expect(spy).toHaveBeenCalledTimes(0);
+	  container.find('.reset-timer');
+	  expect(spy).toHaveBeenCalledTimes(0);
+	});
+  });
+
